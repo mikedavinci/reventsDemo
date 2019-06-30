20a1a4bdb98b6f5fe092f56e9967dd24f86d8892
Miguel Esparza Jr <miguelesparzajr@Miguels-MacBook-Pro.local>
2019-07-01 00:43:31 +0530

    End of Section3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Re-vents</h1>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </Fragment>
     );
   }
 }
 
 export default App;
+
+// This the normal css way of creating a button. The below Semantic-UI React method
+// <button className="ui icon button">
+//   <i className="smile icon" /> CSS Button
+// </button>
+// <Button icon="smile" content="React Button" />
